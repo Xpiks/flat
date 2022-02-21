ccbbb8fed461a0bbe2b4e631238d20ea2f896923
Taras Kushnir <505555+ribtoks@users.noreply.github.com>
2022-02-21 13:21:06 +0200

    Update postprocessing library version

--- postprocess.js
+++ postprocess.js
@@ -2,7 +2,7 @@
 
 // Helper library written for useful postprocessing tasks with Flat Data
 // Has helper functions for manipulating csv, txt, json, excel, zip, and image files
-import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.11/mod.ts' 
+import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.14/mod.ts' 
 
 const filename = Deno.args[0]
 const json = await readJSON(filename)
